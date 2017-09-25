659327a4599510314d40b62627b0687fc2894d4d
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-25 10:54:49 +0700

    menambahkan recycler view

--- app/src/main/java/siukdw/com/contohactivity/CountryAdapter.java
+++ app/src/main/java/siukdw/com/contohactivity/CountryAdapter.java
@@ -1,8 +1,14 @@
 package siukdw.com.contohactivity;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import models.Country;
 
 /**
  * Created by erick on 25/09/2017.
@@ -10,24 +16,41 @@ import android.view.ViewGroup;
 
 public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.MyViewHolder> {
 
+    List<Country> countryList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView tvCountryName;
+        private TextView tvPopulation;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            tvCountryName = (TextView)itemView.findViewById(R.id.tvCountryName);
+            tvPopulation = (TextView)itemView.findViewById(R.id.tvPopulation);
+        }
+    }
+
+    public CountryAdapter(List<Country> countryList){
+        this.countryList = countryList;
+    }
+
     @Override
     public CountryAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_simplerec,parent,false);
+        return new MyViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(CountryAdapter.MyViewHolder holder, int position) {
-
+        Country country = countryList.get(position);
+        holder.tvCountryName.setText(country.getName());
+        holder.tvPopulation.setText(String.valueOf(country.getPopulation()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return countryList.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
-        public MyViewHolder(View itemView) {
-            super(itemView);
-        }
-    }
+
 }
