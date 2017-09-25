7d85399458391909f145d512b768b3611983045b
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-25 10:39:54 +0700

    menambahkan recycler view

---
+++ app/src/main/java/siukdw/com/contohactivity/CountryAdapter.java
@@ -0,0 +1,33 @@
+package siukdw.com.contohactivity;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by erick on 25/09/2017.
+ */
+
+public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.MyViewHolder> {
+
+    @Override
+    public CountryAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(CountryAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
