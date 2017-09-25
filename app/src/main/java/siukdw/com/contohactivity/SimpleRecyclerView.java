e2c15a5bcd18593ad5c180969c1bfaa71e018ad0
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-09-25 10:03:25 +0700

    menambahkan recycler view

---
+++ app/src/main/java/siukdw/com/contohactivity/SimpleRecyclerView.java
@@ -0,0 +1,13 @@
+package siukdw.com.contohactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SimpleRecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_recycler_view);
+    }
+}
