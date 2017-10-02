d6bb33f319d7fa31e4affa43b941a30f5eeae46c
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-10-02 10:26:56 +0700

    menambahkan recycler view

---
+++ app/src/main/java/siukdw/com/contohactivity/DetailCountryActivity.java
@@ -0,0 +1,13 @@
+package siukdw.com.contohactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailCountryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_country);
+    }
+}
